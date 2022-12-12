6a8ba7ac0388cfc9bf564345dc5df0d3a935cdc8
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-12-12 20:21:07 -0300

    Update Program4.java

--- java_completo_POO/manipulandoData_Hora/src/main/java/com/mycompany/manipulandodatas/Program4.java
+++ java_completo_POO/manipulandoData_Hora/src/main/java/com/mycompany/manipulandodatas/Program4.java
@@ -14,13 +14,17 @@ public class Program4 {
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 		
+		// instancia uma data da variavel d04 menos 7 dias.
 		LocalDate pastWeekDate = d04.minusDays(7);
+		// instancia uma data da variavel d04 mais 7 dias.
 		LocalDate nextWeekDate = d04.plusDays(7);
 		
 		LocalDateTime pastWeekLocalDate = d05.minusDays(7);
 		LocalDateTime nextWeekLocalDate = d05.plusDays(7);
 		
+		// instanciando as datas da variavel global d06 menos 7 dias.
 		Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+		// instanciando as datas da variavel global d06 mais 7 dias.
 		Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
 		
 		System.out.println("pastWeekDate = " + pastWeekDate);
@@ -32,6 +36,7 @@ public class Program4 {
 		System.out.println("pastWeekInstant = " + pastWeekInstant);
 		System.out.println("nextWeekInstant = " + nextWeekInstant);
 		
+		// calcular duração entre dois intervalos de datas.
 		Duration t1 = Duration.between(pastWeekDate.atStartOfDay(), d04.atStartOfDay());
 		Duration t2 = Duration.between(pastWeekLocalDate, d05);
 		Duration t3 = Duration.between(pastWeekInstant, d06);
