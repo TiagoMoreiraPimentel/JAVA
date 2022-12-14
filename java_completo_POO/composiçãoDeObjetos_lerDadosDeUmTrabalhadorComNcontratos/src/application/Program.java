bf5cf046f5b30e7bb29c70acea08fa65bc7a7f5f
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-12-14 18:41:44 -0300

    legendas

--- java_completo_POO/composiçãoDeObjetos_lerDadosDeUmTrabalhadorComNcontratos/src/application/Program.java
+++ java_completo_POO/composiçãoDeObjetos_lerDadosDeUmTrabalhadorComNcontratos/src/application/Program.java
@@ -14,11 +14,15 @@ import entities.enums.WorkerLevel;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-
+		
+		// Seta as configurações de teclado dos Estados Unidos.
 		Locale.setDefault(Locale.US);
+		// Instancia o metodo Scanner para capturar dados do teclado do usuario.
 		Scanner sc = new Scanner(System.in);
+		// Instancia a estilização de datas.
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		// Captura os dados.
 		System.out.print("Enter department's name: ");
 		String departmentName = sc.nextLine();
 		System.out.println("Enter worker data:");
@@ -28,19 +32,23 @@ public class Program {
 		String workerLevel = sc.nextLine();
 		System.out.print("Base salary: ");
 		double baseSalary = sc.nextDouble();
+		// Instanciando o trabalhador e o departamento.
 		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
 		
 		System.out.print("How many contracts to this worker? ");
 		int n = sc.nextInt();
 		
+		// laço de repetição que percorre a variavel n.
 		for (int i=1; i<=n; i++) {
 			System.out.println("Enter contract #" + i + " data:");
 			System.out.print("Date (DD/MM/YYYY): ");
+			// coleta a data especificada pelo usuario.
 			Date contractDate = sdf.parse(sc.next());
 			System.out.print("Value per hour: ");
 			double valuePerHour = sc.nextDouble();
 			System.out.print("Duration (hours): ");
 			int hours = sc.nextInt();
+			// instancia a nossa classe de contratos por hora.
 			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
 			worker.addContract(contract);
 		}
@@ -48,8 +56,10 @@ public class Program {
 		System.out.println();
 		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
 		String monthAndYear = sc.next();
+		// realiza a separação do mês e o ano.
 		int month = Integer.parseInt(monthAndYear.substring(0, 2));
 		int year = Integer.parseInt(monthAndYear.substring(3));
+		
 		System.out.println("Name: " + worker.getName());
 		System.out.println("Department: " + worker.getDepartment().getName());
 		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
